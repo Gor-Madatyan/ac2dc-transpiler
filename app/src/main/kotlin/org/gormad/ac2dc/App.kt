3f51eae947bafe76adcac794cd7de74420020ce0
Gor Madatyan <gor-madatyan@proton.me>
2026-03-18 23:23:30 +0400

    Implement file transpilation

--- app/src/main/kotlin/org/gormad/ac2dc/App.kt
+++ app/src/main/kotlin/org/gormad/ac2dc/App.kt
@@ -1,14 +1,21 @@
 package org.gormad.ac2dc
 
 import org.gormad.ac2dc.utils.getTranspiller
+import java.nio.file.Files
+import java.nio.file.Path
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    if(args.size != 2){
+    if(args.size != 3){
         System.err.println("not enough arguments")
+        System.err.println("Usage: <plugin-path> <src> <output>")
         exitProcess(1)
     }
-
     val transpiler = getTranspiller(args[0])
-    println(transpiler.transpile(args[1]))
+
+
+    var lines = Files.readAllLines(Path.of(args[1]))
+    lines = lines.map { line -> transpiler.transpile(line.trim())+'\n'}
+
+    Files.write(Path.of(args[2]), lines)
 }
